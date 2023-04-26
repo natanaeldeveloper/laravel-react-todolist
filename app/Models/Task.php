ec295ba830e3f71d8687cc305c18caf592181687
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-04-25 21:33:54 -0300

    feat(Task.php): added fillables and relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id_user',
+        'description',
+        'date_conclusion',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
